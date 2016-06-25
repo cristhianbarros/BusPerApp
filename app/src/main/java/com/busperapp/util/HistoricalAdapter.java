ed2adf8fee481987514c1ddd7467ddbf03064e39
agrajava <alvaroandres0608@gmail.com>
2016-06-25 16:33:15 -0500

    Se actualiza CardView con menus

--- app/src/main/java/com/busperapp/util/HistoricalAdapter.java
+++ app/src/main/java/com/busperapp/util/HistoricalAdapter.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Priority;
 import com.bumptech.glide.load.data.DataFetcher;
@@ -39,13 +44,14 @@ public class HistoricalAdapter extends RecyclerView.Adapter<HistoricalAdapter.Vi
         private TextView title;
         private TextView descripcion;
         private ImageView imagen;
+        private ImageView overflow;
 
         public ViewHolder(View v) {
             super(v);
             title = (TextView) v.findViewById(R.id.title);
             descripcion =(TextView) v.findViewById(R.id.descripcion);
             imagen = (ImageView) v.findViewById(R.id.imagen);
-
+            overflow = (ImageView) v.findViewById(R.id.overflow);
         }
     }
 
@@ -70,12 +76,49 @@ public class HistoricalAdapter extends RecyclerView.Adapter<HistoricalAdapter.Vi
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent I = new Intent(context,DetailObjectActivity.class);
+                Intent I = new Intent(context, DetailObjectActivity.class);
                 I.putExtra("key", ObjHistorical.getKey());
                 context.startActivity(I);
             }
         });
+
+        holder.overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(holder.overflow);
+            }
+        });
     }
+
+    private void showPopupMenu(View view) {
+        // inflate menu
+        PopupMenu popup = new PopupMenu(context, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_historical, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
+    }
+
+    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.edit:
+                    Toast.makeText(context, "Editar", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.delete:
+                    Toast.makeText(context, "Borrar", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
+        }
+    }
+
 
     @Override
     public int getItemCount() {

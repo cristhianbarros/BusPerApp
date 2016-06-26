4c53e0643db33f8e19f534f3b07bee47dcad81a3
agrajava <alvaroandres0608@gmail.com>
2016-06-26 16:43:28 -0500

    Se realiza validacion en el logui y se actualiza el content registrer

--- app/src/main/java/com/busperapp/login/ui/LoginActivity.java
+++ app/src/main/java/com/busperapp/login/ui/LoginActivity.java
@@ -4,14 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.busperapp.MainActivity;
 import com.busperapp.R;
 import com.busperapp.login.LoginPresenter;
